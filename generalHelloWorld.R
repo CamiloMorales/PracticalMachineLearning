0dd848b7204cc022558fc4609040e05cee3fe845
“CamiloMorales” <camilo.morales@gmail.com>
2015-08-15 22:30:57 +0200

    New mean line.

--- generalHelloWorld.R
+++ generalHelloWorld.R
@@ -5,3 +5,4 @@ inTrain <- createDataPartition(y=spam$type, p=0.75, list=FALSE);
 training <- spam[inTrain,];
 testing <- spam[-inTrain,];
 hist(training$capitalAve, main="", xlab="ave. capital run length")
+mean(training$capitalAve)
